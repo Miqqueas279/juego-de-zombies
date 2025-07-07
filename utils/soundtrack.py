7c6e5c032ac86bfa8d65f27c3e858332be9b2cdd
Federico Piretro <federicopiretro@outlook.com>
2025-07-07 19:28:24 -0300

    documentacion funciones

--- utils/soundtrack.py
+++ utils/soundtrack.py
@@ -2,6 +2,10 @@ import os
 import pygame
 
 def play_music(music_name: str, volume: float) -> None:
+    """
+    Ingresa el nombre de la musica conjunto al volumen deseado y comprueba si existe
+    Si es asi, lo reproduce infinitamente y sino printea un mensaje para que no rompa el juego.
+    """
     music_path = os.path.join("assets", "sounds", music_name)
 
     if os.path.exists(music_path):
@@ -12,9 +16,16 @@ def play_music(music_name: str, volume: float) -> None:
         print(f"[ERROR] Archivo de música no encontrado: {music_path}") 
     
 def stop_music() -> None:
+    """
+    Detiene la musica
+    """
     pygame.mixer.music.stop()
 
 def load_sound(sound_name: str, volume: float) -> pygame.mixer.Sound | None:
+    """
+    Ingresa el nombre del sonido conjunto al volumen deseado y comprueba si existe
+    Si es asi, lo reproduce y sino printea un mensaje para que no rompa el juego.
+    """
     sound_path = os.path.join("assets", "sounds", sound_name)
 
     if os.path.exists(sound_path):
